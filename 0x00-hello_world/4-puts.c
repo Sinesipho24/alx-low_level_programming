aac4d833889b9ea05412470854a631c0b397e300
Sinesipho siyaka <Siyakasinesipho@gmai.com>
2022-09-12 05:52:40 -0700

    4 - Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-* main - prints string in the put function
-*
-*Description
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
+*int main(void)
+*{
+*puts(\"Programming is like building a multilingual puzzle)
+;
+Return(0);
+}
