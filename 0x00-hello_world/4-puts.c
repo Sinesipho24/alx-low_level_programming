31f66b00a203dd9107a25ee43554d4dd5e1157a8
Sinesipho siyaka <Siyakasinesipho@gmai.com>
2022-09-12 04:18:57 -0700

    0 - preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - prints string in the put function
+*
+*Description
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
